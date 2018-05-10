13a149685ba451e7133a606261a41a882b697f84
venkatesh v <venkatesh_V@trimble.com>
2018-05-10 19:00:48 +0530

    Commiting Tour of Heroes Deveolpment

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  constructor() { }
+  hero: Hero = {
+    id: 1,
+    name: 'Captain America'
+  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  ngOnInit() {
+  }
+}
